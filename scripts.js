d23d8a5f913720db9bb416ff76713b070be9648a
Maurice Hobbs <mauricehobbs379@gmail.com>
2019-07-22 19:39:06 -0400

    intialupload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function navbar(){
+    
+}
+function openxboxpage (){
+    location.href="xboxone.html"
+    
+}function openps4page(){
+    location.href="playstation.html"
+}
+if (confirm("Are you a Xbox gamer ?")) {
+   openxboxpage ()
+}
+else if (confirm("Are you a Playstation gamer ?")){
+    openps4page ()
+}
